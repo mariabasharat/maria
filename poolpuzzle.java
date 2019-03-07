b50a10938a4cffa63980bb3fd445c03408839475
mariabasharat <mariabasharat33105@gmail.com>
2019-03-07 10:37:56 +0500

    1st

---
+++ poolpuzzle.java
@@ -0,0 +1,28 @@
+public class poolpuzzleone {
+public static void main(string[] args) {
+int x=0;
+while(x<1){
+system.out.println("a");
+system.out.print("noise");
+if(x<1){
+system.out.println("annoys");
+
+}
+
+if(x>3){
+system.out.println("noys")'
+x==x-1;
+}
+if(x==1){
+system.out.println("oise")'
+
+}
+if(x<4){
+system.out.prinln("an")'
+system.out.println("oydter")'
+}
+system.out.println(" ")'
+x=x+2'
+}
+}
+}
