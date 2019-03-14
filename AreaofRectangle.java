0d22fe13c891c9daece9be6e684ee7fc8b7f8bcb
mariabasharat <48020460+mariabasharat@users.noreply.github.com>
2019-03-14 21:02:51 +0500

    git commit

---
+++ AreaofRectangle.java
@@ -0,0 +1,15 @@
+class AreaofRectangle {
+public static void main (string[] args)
+{
+     Scanner scanner = new
+scanner(system.in);
+system.out.println("enter the length of the rectangle:");
+    double length=
+scanner.nextDouble();
+system.out.println("enter the width of rectangle:");
+double width=
+scanner .nextDouble();
+double area = length*width;
+system.out.println('Area of the rectangle is:"+area);
+}
+}
