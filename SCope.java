3d21368f44baa58aed6f3f48002ff15db7d2357a
mariabasharat <48020460+mariabasharat@users.noreply.github.com>
2019-03-14 23:14:32 +0500

    git commit

---
+++ SCope.java
@@ -0,0 +1,22 @@
+public class SCope {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n,d,q;
+        n=10;
+        d=2;
+        if(d!=0&&(n%d)==0)
+            System.out.println(d+"is a factor of"+n);
+        d=0;
+        if(d!=0&&(n%d)==0)
+              System.out.println(d+"is a factor of"+n);
+          if(d!=0&(n%d)==0)
+              System.out.println(d+"is a factor of"+n);
+        
+            
+        // TODO code application logic here
+    }
+    
+}
