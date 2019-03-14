6d9d5bffe04b206c316d90378ddc8ca5cdfee6b7
mariabasharat <48020460+mariabasharat@users.noreply.github.com>
2019-03-14 22:18:41 +0500

    git commit

---
+++ maria.java
@@ -0,0 +1,12 @@
+public class Maria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // TODO code application logic here
+    }
+    
+}
+
