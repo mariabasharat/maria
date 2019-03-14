00b3335f05fd46ab8b29e8d92f67668d7e2d4190
mariabasharat <48020460+mariabasharat@users.noreply.github.com>
2019-03-14 09:44:05 +0500

    git commit

---
+++ ScopeDemo.java
@@ -0,0 +1,12 @@
+class ScopeDemo{
+public staatic void main(string args []){
+int x;
+x=10;
+if(x==10){
+int y=20;
+system.out.println(" x and y: " + x "" + y);
+x=y * 2;
+}
+system.out.println("x is " + x );
+}
+}
