b15482ee53b2bf619a6b4d5c338dd2f70e331d64
mariabasharat <48020460+mariabasharat@users.noreply.github.com>
2019-03-14 22:58:36 +0500

    git commit

---
+++ PromDemo.java
@@ -0,0 +1,37 @@
+public class PromDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        int i;
+        b=10;
+        i=b*b;
+        b=10;
+        b=(byte) (b*b);
+        System.out.println("i and b:"+i+" "+b);
+        
+        
+        
+                
+                
+                
+                
+                
+                
+                
+                
+              
+                
+                
+                
+                
+                
+                
+                
+        
+        // TODO code application logic here
+    }
+    
+}
