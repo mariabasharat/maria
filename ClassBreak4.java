5ab37cf5021f08a698b81f8e9c27234cfbe93b76
mariabasharat <48020460+mariabasharat@users.noreply.github.com>
2019-05-08 21:25:55 +0500

    git commit

---
+++ ClassBreak4.java
@@ -0,0 +1,24 @@
+// using break with nested loops.
+ClassBreak4{
+public static void main (string args [] )
+int i;
+for (int i=1; i<4; i++){
+{
+  {
+     {
+system.out,println("\ni is " + i); 
+if(i==1) break one;
+if (i==2) break two;
+if(i==3) break three;
+          system.out.println("won't print ");
+       }
+      system.out.println("after block three. ");
+     }
+    system.out.println("after block two.");
+   }
+  system.out.println("after block one.");
+ }
+system.out.println("after for.");
+}
+}
+
